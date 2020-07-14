1e7506ccf8480c428262f91608ef64999a479c26
Matt O'Connor <matthew.oconnor@smartapprentices.com>
2020-07-14 08:56:59 +0100

    End of 182

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -14,6 +14,7 @@ import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import NotFound from './NotFound';
 import {ToastContainer} from 'react-toastify';
+import LoginForm from '../../features/user/LoginForm';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
@@ -29,6 +30,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
               <Switch>
                 <Route exact path='/activities' component={ActivityDashboard} />
                 <Route path='/activities/:id' component={ActivityDetails} />
+                <Route path='/login' component={LoginForm} />
                 <Route
                   key={location.key}
                   path={['/createActivity', '/manage/:id']}
